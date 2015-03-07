197f7eba84c31a553f4a3223224a43ee440ab22d
cryingMouse <snaomi4649@gmail.com>
2015-03-07 17:31:50 +0900

    no message

---
+++ a20140511/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDepth = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTest = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTest2 = new System.Windows.Forms.PictureBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTest2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDepth
+            // 
+            this.pictureBoxDepth.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxDepth.Location = new System.Drawing.Point(12, 29);
+            this.pictureBoxDepth.Name = "pictureBoxDepth";
+            this.pictureBoxDepth.Size = new System.Drawing.Size(495, 378);
+            this.pictureBoxDepth.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDepth.TabIndex = 0;
+            this.pictureBoxDepth.TabStop = false;
+            // 
+            // pictureBoxTest
+            // 
+            this.pictureBoxTest.Location = new System.Drawing.Point(600, 212);
+            this.pictureBoxTest.Name = "pictureBoxTest";
+            this.pictureBoxTest.Size = new System.Drawing.Size(287, 195);
+            this.pictureBoxTest.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxTest.TabIndex = 1;
+            this.pictureBoxTest.TabStop = false;
+            // 
+            // pictureBoxTest2
+            // 
+            this.pictureBoxTest2.Location = new System.Drawing.Point(600, 29);
+            this.pictureBoxTest2.Name = "pictureBoxTest2";
+            this.pictureBoxTest2.Size = new System.Drawing.Size(287, 177);
+            this.pictureBoxTest2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxTest2.TabIndex = 2;
+            this.pictureBoxTest2.TabStop = false;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonSave.Location = new System.Drawing.Point(513, 29);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 53);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "記録";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.textBox1.Location = new System.Drawing.Point(513, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "test";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(513, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "被験者名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(516, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "条件";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "0",
+            "15",
+            "30",
+            "left",
+            "right",
+            "back"});
+            this.listBox1.Location = new System.Drawing.Point(513, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "-15",
+            "-10",
+            "-5",
+            "0",
+            "5",
+            "10",
+            "15",
+            "swing",
+            "swing_low",
+            "swing_high"});
+            this.comboBox1.Location = new System.Drawing.Point(512, 296);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 419);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.pictureBoxTest2);
+            this.Controls.Add(this.pictureBoxTest);
+            this.Controls.Add(this.pictureBoxDepth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTest2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDepth;
+        private System.Windows.Forms.PictureBox pictureBoxTest;
+        private System.Windows.Forms.PictureBox pictureBoxTest2;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ a20140511/Form1.cs
@@ -0,0 +1,459 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using OpenCvSharp;
+using System.Drawing.Imaging;
+using System.Threading;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.CPlusPlus;
+using CPP = OpenCvSharp.CPlusPlus;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.IO;
+using CenterOfGravity;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public const int Bgr32BytesPerPixel = 4;    //ピクセルあたりのバイト数
+        const string COLOR = "_color";
+        const string DEPTH = "_depth";
+        const string SKELETON = "_skeleton";
+        const string COLOR_TO_DEPTH = "_point";
+        const string PRESSURE = "";
+        IplImage image;
+
+        int saveNumber = 0;
+        int count = 0;
+        const int CountMax = 100;
+        const int DepthMax = 4000;
+        DirectoryInfo di;
+        string filename = "";
+        
+        /*----被験者-----------*/
+        
+
+        string subject = "test";
+        string condition = "0";
+
+
+        /*---------------------*/
+
+        //深度データ
+        bool saving = false;
+        bool output = false;
+        int[] depth;
+        //貯める
+        long[] save_timestamp; //タイムスタンプ
+        //color
+        int[,] save_depth;  //depth
+        DepthImagePoint[,] save_colorToDepth; //colorToDepth
+        DepthImagePoint[,] save_skeleton; //skeletonToDepth
+        Save save_pressure; //pressure
+        
+        public Form1()
+        {
+            InitializeComponent();
+            if (KinectSensor.KinectSensors.Count < 1)
+            {
+                Console.WriteLine("kinectをもっと接続してください");
+            }
+            //ファイル名
+            DateTime today = DateTime.Now;
+            string mo, d, h, m, s;
+            if (today.Month < 10)
+                mo = "0" + today.Month.ToString();
+            else mo = today.Month.ToString();
+            if (today.Day < 10)
+                d = "0" + today.Day.ToString();
+            else d = today.Day.ToString();
+            if (today.Hour < 10)
+                h = "0" + today.Hour.ToString();
+            else h = today.Hour.ToString();
+            if (today.Minute < 10)
+                m = "0" + today.Minute.ToString();
+            else m = today.Minute.ToString();
+            if (today.Second < 10)
+                s = "0" + today.Second.ToString();
+            else s = today.Second.ToString();
+            filename = today.Year.ToString() + mo + d + h + m + s;
+            //被験者名・実験条件はあとで足す
+
+            //ディレクトリ作成
+            di = Directory.CreateDirectory(filename);
+
+            image = Cv.CreateImage(new CvSize(640, 480), BitDepth.U8, 1);
+            save_timestamp = new long[CountMax];
+            save_depth = new int[CountMax, 640 * 480];
+            save_skeleton = new DepthImagePoint[CountMax, 20];
+            save_colorToDepth = new DepthImagePoint[CountMax, 640 * 480];
+            save_pressure = new Save("COM8");
+            Thread threadStart = new Thread(new ParameterizedThreadStart(thrStart));
+            threadStart.Start();
+            threadStart.Join();
+            //KinectSensor.KinectSensors[0].AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(kinect_AllFramesReady);
+        }
+
+        private void thrStart(object o)
+        {
+            startKinect(KinectSensor.KinectSensors[0], 0);
+        }
+
+        private void startKinect(KinectSensor kinect, int kinectNumber)
+        {
+            //kinect.DepthStream.Range = DepthRange.Near;
+            kinect.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+            kinect.ColorStream.Enable();
+            kinect.DepthStream.Enable();
+            kinect.SkeletonStream.Enable();
+            kinect.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(kinect_AllFramesReady);
+            kinect.Start();
+        }
+
+        unsafe void kinect_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            try
+            {
+                KinectSensor kinectSensor = sender as KinectSensor;
+                if (kinectSensor == null) return;
+                using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+                using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+                using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+                {
+                    if (colorFrame != null && depthFrame != null && skeletonFrame != null)
+                    {
+                        /* タイムスタンプ */
+                        long timestamp = depthFrame.Timestamp;
+                        //Console.WriteLine(timestamp);
+
+                        /* RGBデータ取得 */
+                        byte[] colorPixel = new byte[colorFrame.PixelDataLength];
+                        colorFrame.CopyPixelDataTo(colorPixel);
+                        IplImage srcImg = IplImage.FromPixelData(new CvSize(colorFrame.Width, colorFrame.Height), 4, colorPixel);
+
+                        /* 距離データを取得 */
+                        DepthImagePixel[] dip = new DepthImagePixel[depthFrame.PixelDataLength];
+                        depthFrame.CopyDepthImagePixelDataTo(dip);
+
+                        /* 距離データのみ保存 */
+                        short[] depthPixel = new short[depthFrame.PixelDataLength];
+                        depthFrame.CopyPixelDataTo(depthPixel);
+
+                        /* 距離に変換 */
+                        int[] distance = new int[depthFrame.PixelDataLength];
+                        for (int i = 0; i < depthPixel.Length; i++)
+                        {
+                            distance[i] = dip[i].Depth;
+                            
+                        }
+                        
+
+                        /* RGB画像を深度画像にマッピング */
+                        DepthImagePoint[] colorPoint = new DepthImagePoint[colorFrame.PixelDataLength / colorFrame.BytesPerPixel];
+                        DepthImageFormat dp = DepthImageFormat.Resolution640x480Fps30;
+                        kinectSensor.CoordinateMapper.MapColorFrameToDepthFrame(ColorImageFormat.RgbResolution640x480Fps30,
+                                                                                dp,
+                                                                                dip, colorPoint);
+
+                        /* 距離データ画像化*/
+                        toIplImage(distance, depthFrame.Width, depthFrame.Height, kinectSensor.UniqueKinectId);
+                        
+
+                        /* スケルトン */
+                        Skeleton[] skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                        skeletonFrame.CopySkeletonDataTo(skeletons);
+                        DepthImagePoint[] jointPoints = new DepthImagePoint[skeletons[0].Joints.Count];
+                        if (skeletons[0].TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            /* 1人目のプレイヤーについて関節取得 */
+                            Joint[] joints = new Joint[skeletons[1].Joints.Count];
+
+                            byte* p = (byte*)image.ImageData;
+                            foreach (JointType jointType in Enum.GetValues(typeof(JointType)))
+                            {
+                                int i = (int)jointType;
+                                jointPoints[i] = kinectSensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skeletons[0].Joints[jointType].Position, DepthImageFormat.Resolution640x480Fps30);
+
+                            }
+                            this.BackColor = Color.Black;
+
+                        }
+                        else
+                        {
+                            this.BackColor = Color.White;
+                        }
+
+                        /* セーブ中だったら */
+                         if (saving)
+                        {
+                            if (count < CountMax)
+                            {
+                                Console.WriteLine(count);
+                                save_timestamp[count] = timestamp;//timestamp
+                                Cv.SaveImage(@di.FullName + "\\" + filename + subject + condition /* + "_" + saveNumber.ToString() */ +COLOR + "_" + count.ToString() + ".bmp", srcImg);//color
+                                for (int i = 0; i < 640*480; i++)
+                                {
+                                    save_depth[count, i] = distance[i]; //depth
+                                    save_colorToDepth[count, i] = colorPoint[i];//colorToDepth
+                                }
+                                for (int i = 0; i < jointPoints.Count(); i++)
+                                {
+                                    save_skeleton[count, i] = jointPoints[i];//skeleton
+                                }
+                                
+                                save_pressure.memData();//pressure
+
+                                count++;
+                            }
+                            else
+                            {
+                                saving = false;
+                                /*
+                                 *  スレッド・セーブ
+                                 *  データアウトプット
+                                 * 
+                                 */
+                                Thread threadSave = new Thread(new ParameterizedThreadStart(thrSave));
+                                threadSave.Start();
+                                count = 0;
+                            }
+                        }
+
+                        pictureBoxDepth.Image = image.ToBitmap();
+                        
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("framerady"+ ex.Message);
+            }
+            
+        }
+
+        private void thrSave(object o)
+        {
+            /*
+             
+               save処理
+             
+             
+             */
+            //color 都度セーブしてるので今回しない
+            //depth
+            Thread threadSaveDepth = new Thread(new ParameterizedThreadStart(thrSaveDepth));
+            //skeleton
+            Thread threadSaveSkeleton = new Thread(new ParameterizedThreadStart(thrSaveSkeleton));
+            //colorToDepth
+            Thread threadSaveColorToDepth = new Thread(new ParameterizedThreadStart(thrSaveColorToDepth));
+            //pressure
+            Thread threadSavePressure = new Thread(new ParameterizedThreadStart(thrSavePressure));
+            Console.WriteLine("save start.");
+            threadSaveDepth.Start();
+            threadSaveSkeleton.Start();
+            threadSaveColorToDepth.Start();
+            threadSavePressure.Start();
+
+            threadSaveDepth.Join();
+            threadSaveSkeleton.Join();
+            threadSaveColorToDepth.Join();
+            threadSavePressure.Join();
+
+            saveNumber++;
+        }
+
+        private void thrSaveDepth(object o)
+        {
+            DataTableToCsv(save_depth, @di.FullName + "\\" + filename + subject + condition /* + "_" + saveNumber.ToString() */ + DEPTH, save_timestamp);
+            Console.WriteLine("depth");
+        }
+        private void thrSaveSkeleton(object o)
+        {
+            DataTableToCsv(save_skeleton, @di.FullName + "\\" + filename + subject + condition /* + "_" + saveNumber.ToString() */ + SKELETON, save_timestamp);
+            Console.WriteLine("skeleton");
+
+        }
+        private void thrSaveColorToDepth(object o)
+        {
+            DataTableToCsv(save_colorToDepth, @di.FullName + "\\" + filename + subject + condition /* + "_" + saveNumber.ToString() */ + COLOR_TO_DEPTH, save_timestamp);
+            Console.WriteLine("color to depth");
+        }
+        private void thrSavePressure(object o)
+        {
+            save_pressure.saveData(@di.FullName + "\\" + filename + subject + condition /* + "_" + saveNumber.ToString() */ + PRESSURE);
+            Console.WriteLine("pressure");
+        }
+
+        static public void DataTableToCsv(int[,] dt, string filePath, long[] timestamp)
+        {
+            StringBuilder sb = new StringBuilder();
+            System.IO.StreamWriter sw = null;
+            List<int> filterIndex = new List<int>();
+
+            try
+            {
+                sw = new System.IO.StreamWriter(filePath + ".csv", false, System.Text.Encoding.GetEncoding("Shift_JIS"));
+
+                //----------------------------------------------------------//
+                // 内容を出力します。                                       //
+                //----------------------------------------------------------//
+                for (int j=0; j<CountMax;j++)
+                {
+                    //タイムスタンプ
+                    sb.Append(timestamp[j].ToString());
+                    sb.Append(",");
+                    //中身
+                    for (int i = 0; i < 640*480; i++)
+                    {
+                        sb.Append(dt[j,i].ToString().Replace("\"", "\"\""));
+                        sb.Append(",");
+                    }
+                    sb.Append("\r");
+                    sw.Write(sb.ToString());
+                    sb.Clear();
+                }
+                
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+            }
+        }
+        static public void DataTableToCsv(DepthImagePoint[,] dt, string filePath, long[] timestamp)
+        {
+            StringBuilder sb = new StringBuilder();
+            System.IO.StreamWriter sw = null;
+            List<int> filterIndex = new List<int>();
+
+            try
+            {
+                sw = new System.IO.StreamWriter(filePath + ".csv", false, System.Text.Encoding.GetEncoding("Shift_JIS"));
+
+                //----------------------------------------------------------//
+                // 内容を出力します。                                       //
+                //----------------------------------------------------------//
+                for (int j = 0; j < CountMax; j++)
+                {
+                    //タイムスタンプ
+                    sb.Append(timestamp[j].ToString());
+                    sb.Append(",");
+                    //中身
+                    int length = dt.Length / CountMax;
+                    for (int i = 0; i < length; i++)
+                    {
+                        sb.Append(dt[j, i].X.ToString().Replace("\"", "\"\""));
+                        sb.Append(",");
+                        sb.Append(dt[j, i].Y.ToString().Replace("\"", "\"\""));
+                        sb.Append(",");
+                        sb.Append(dt[j, i].Depth.ToString().Replace("\"", "\"\""));
+                        sb.Append(",");
+                    }
+                    sb.Append("\r");
+                    sw.Write(sb.ToString());
+                    sb.Clear();
+                }
+                
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+            }
+        }
+
+        public unsafe void toIplImage(int[] d, int width, int height, string id)
+        {
+            IplImage imageDepth =  null;
+            int[] depth;
+            try
+            {
+                depth = (int[])d.Clone();
+
+                imageDepth = Cv.CreateImage(new CvSize(width, height), BitDepth.U8, 1);
+                byte* p = (byte*)imageDepth.ImageData;
+                int min, max;
+
+                //とりあえずしょり
+                for (int n = 0; n < depth.Length; n++)
+                {
+
+                    if (depth[n] > DepthMax || depth[n] < 400)
+                    {
+                        depth[n] = 0;
+                    }
+                    else
+                    {
+                        depth[n] = DepthMax - depth[n];
+                    }
+                }
+                min = depth.Min();
+                max = depth.Max();
+                //画像化
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+
+                        p[y * width + x] = (byte)(255 * depth[y * width + x] / (max - min));
+
+                    }
+                }
+
+                image = imageDepth.Clone();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (imageDepth != null) imageDepth.Dispose();
+                
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            saving = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            subject = textBox1.Text;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            condition = "_" + listBox1.SelectedItem.ToString();
+        }
+
+    }
+}
